0922219a792799438b77547ded2f90b307ba665b
Tõnis <tonis-ingvar.mals@khk.ee>
2019-04-08 10:15:43 +0300

    01 Write a unit test

---
+++ tests/ReceiptTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace TDD\Test;
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use TDD\Receipt;
+
+class ReceiptTest extends TestCase {
+    public function testTotal() {
+        $Receipt = new Receipt(); // Loome objekti
+        $this->assertEquals( //PHPUniti testi meetod
+            14, // oodatav tulemus
+            $Receipt->total([0,2,5,8]), //Kutsume objektist meetodi Total ja anname ette massiivi numbritest
+            'When summing the total should equal 15' //Vea puhul tagastatav teade
+        );
+    }
+}
